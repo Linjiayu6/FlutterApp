56344f611e0554404ee5d9ef4e87b7aebf0b4a93
linjiayu <linjiayu@meituan.com>
2019-07-08 20:23:26 +0800

    聊天完善

--- lib/pages/chatWindow.dart
+++ lib/pages/chatWindow.dart
@@ -34,20 +34,22 @@ class ChatWindowState extends State<ChatWindow> {
   Widget _renderBody(context, _messages) {
     return Column(
       children: <Widget>[
-        // 用户输入的输出内容列表
+        // 展示区域: 用户输入的输出内容列表
         Flexible(
           child: ListView.builder(
             padding: EdgeInsets.all(6.0),
             reverse: true, // 从下往上显示
             itemCount: _messages.length,
             itemBuilder: (BuildContext context, int index) {
-              return Text(_messages[index].text);
+              return _renderChatItem(context, _messages[index].text);
             }
           )
         ),
 
+        // 一个分界线
         Divider(height: 1.0),
 
+        // 输入区域: 左边是输入框 右边是Submit
         Container(
           padding: EdgeInsets.only(top: 3.0, bottom: 20.0, right: 10.0, left: 20.0),
           child: Row(
@@ -55,6 +57,10 @@ class ChatWindowState extends State<ChatWindow> {
               Flexible(
                 child: TextField(
                   controller: _controller,
+                  autocorrect: true, // 是否自动更正
+                  maxLines: 1, // 最大行数
+                  autofocus: true, // 是否自动对焦
+                  style: TextStyle(fontSize: 15.0, color: Colors.grey[800]),//输入文本的样式
                   decoration: InputDecoration(
                     hintText: 'Please Type ...',
                   ),
@@ -81,7 +87,45 @@ class ChatWindowState extends State<ChatWindow> {
     );
   }
 
+
+  Widget _renderChatItem(context, _msgItem) {
+    return Container(
+      // 文字间的间距
+      margin: EdgeInsets.symmetric(vertical: 5.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // 头像区域
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(child: Text('a')),
+          ),
+
+          // 名字和用户输入内容
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                // Text('名字'),
+                Container(
+                  color: Colors.grey[300],
+                  padding: EdgeInsets.all(6.0),
+                  margin: EdgeInsets.only(top: 5.0, right: 50.0),
+                  child: Text(_msgItem),
+                )
+              ]
+            )
+          )
+        ]
+      )
+    );
+  }
+
+  // 输入信息处理
   void _handleSubmitted(String text) {
+    if (text.length == 0) {
+      return null;
+    }
     // 输入完后, 清空输入内容
     _controller.clear();
 
