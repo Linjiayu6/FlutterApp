296b4c9f3a62c167ef2fcae836594be0d34f5c40
linjiayu <linjiayu@meituan.com>
2019-07-10 16:32:31 +0800

    del codes

--- lib/pages/contacts.dart
+++ lib/pages/contacts.dart
@@ -1,15 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-
-void getHttp() async {
-  try {
-    Response response = await Dio().get('http://39.100.116.12:8080/flutter/chatlist');
-    print(response);
-  } catch (e) {
-    print(e);
-  }
-}
 
 class Contacts extends StatefulWidget {
   Contacts({ Key key}) : super(key: key);
@@ -20,7 +10,6 @@ class Contacts extends StatefulWidget {
 class ContactsState extends State<Contacts> {
   @override
   Widget build(BuildContext context) {
-    getHttp();
     return Center(
       child: Text('Contacts Widget')
     );
